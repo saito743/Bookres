2239f1e13c9241e67872f5c0d8923b3671216e3f
vagrant <vagrant@localhost.localdomain>
2020-08-17 11:57:16 +0000

    [Update]投稿ページ・一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+
+  get 'books' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books/index' => 'books#index'
+  get 'books/:id' => 'books#show'
+  patch 'books/:id/edit' => 'books#edit', as:'edit_books'
+  delete 'books/id' => 'books#destroy', as:'destroy_books'
+
 end
